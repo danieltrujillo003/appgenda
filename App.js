eeb85b028ec1ac98001d2dc970c15ec88ff5fc6c
dcadavidzuleta <58377455+dcadavidzuleta@users.noreply.github.com>
2020-05-07 10:45:23 -0500

    Screen edit

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './screens/home'
 import List from './screens/list'
+import Edit from './screens/edit'
+
 
 const Stack = createStackNavigator();
 
@@ -11,8 +13,11 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="List" component={List} />
+       {/* <Stack.Screen name="Home" component={Home} />*/}
+        {/*<Stack.Screen name="List" component={List} />*/}
+       {/* <Stack.Screen name="Create" component={Create} /> */}
+        {/*<Stack.Screen name="Details" component={Details} /> */}
+        <Stack.Screen name="Edit" component={Edit} /> 
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/edit.js
@@ -0,0 +1,82 @@
+import React, {useEffect, useState, Component} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { TouchableHighlight,  FlatList} from 'react-native-gesture-handler';
+import { useIsFocused } from '@react-navigation/native';
+
+function EditAppointments({ navigation }) {
+    const isFocused = useIsFocused();
+    const [appointments, setAppointments, value, onChangeText] = useState([]);
+    /* Data for the flatlist */
+    const fetchAppointments = async () =>{
+        let response = await fetch('http://localhost/appgenda-api-slim/api/appointments');
+        let jsonResponse = await response.json();
+        setAppointments(jsonResponse);
+    }
+    useEffect(()=>{
+        fetchAppointments();
+    },[isFocused]);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Datos personales:</Text>
+            <TextInput style={styles.input} value= {""} onChangeText={text=> onChangeText(text)}/> 
+            <TextInput style={styles.input} value= {""} onChangeText={text => onChangeText(text)}/>
+            <TextInput style={styles.input} value= {""} onChangeText={text => onChangeText(text)}/>
+            
+            <Text style={styles.title}>Datos del cliente:</Text>
+            <TextInput style={styles.input} value= {""} onChangeText={text=> onChangeText(text)}/> 
+            <TextInput style={styles.input} value= {""} onChangeText={text => onChangeText(text)}/>
+            <TextInput style={styles.input} value= {""} onChangeText={text => onChangeText(text)}/>
+            <TextInput style={styles.input} value= {""} onChangeText={text=> onChangeText(text)}/> 
+            <TextInput style={styles.input} value= {""} onChangeText={text => onChangeText(text)}/>
+            <TextInput style={styles.input} value= {""} onChangeText={text => onChangeText(text)}/>
+            <TextInput style={styles.input} value= {""} onChangeText={text=> onChangeText(text)}/> 
+            <TextInput style={styles.input} value= {""} onChangeText={text => onChangeText(text)}/>
+            
+            <View style={styles.buttons}>
+                <Button title='Save' />
+                <Button title='Cancel' />
+            </View>
+
+            <FlatList
+                data={appointments}
+                renderItem={({ item }) => <CardComponent appointment={item}/>}
+                keyExtractor={item => item.id}>
+            </FlatList>   
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        paddingLeft: 15,
+        paddingRight: 15,
+        marginTop: 30
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    buttons: {
+        marginTop: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+      },
+    input: {
+        height: 40,
+        borderColor: 'black',
+        borderRadius: 10,
+        berderWidth: 2,
+        marginBottom: 20
+    },
+    title: {
+        fontSize: 32,
+    },
+});
+
+export default EditAppointments;
